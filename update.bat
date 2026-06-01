3ef0f0a2c547425b153616ec7b787e0e45ca55b4
pinkaroo <pinkaroothepro@gmail.com>
2026-06-01 22:48:38 +0800

    release: update installer v1.0.3

--- update.bat
+++ update.bat
@@ -7,7 +7,7 @@ for /f "tokens=1,2,3 delims=." %%a in (version.txt) do (
     set /a PATCH=%%c+1
 )
 
-echo !MAJOR!.!MINOR!.!PATCH! > version.txt
+(echo !MAJOR!.!MINOR!.!PATCH!)> version.txt
 
 git add -A
 git commit -m "release: update installer v!MAJOR!.!MINOR!.!PATCH!"

bbed048b3c46c500858ed023f17b6a33646752a9
pinkaroo <pinkaroothepro@gmail.com>
2026-06-01 22:43:32 +0800

    release: update installer v1.0.1

---
+++ update.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal enabledelayedexpansion
+
+for /f "tokens=1,2,3 delims=." %%a in (version.txt) do (
+    set MAJOR=%%a
+    set MINOR=%%b
+    set /a PATCH=%%c+1
+)
+
+echo !MAJOR!.!MINOR!.!PATCH! > version.txt
+
+git add -A
+git commit -m "release: update installer v!MAJOR!.!MINOR!.!PATCH!"
+git push origin main
+
+echo Done! Pushed v!MAJOR!.!MINOR!.!PATCH!
+pause
